f7cdf130b46f1ae97eb4aa894f57097deaef75e0
Ronald oliveira <ronaldoliveirajeronimo@gmail.com>
2025-06-19 20:31:42 -0300

    Projeto conta banco

---
+++ Conta-Banco/src/ContaTerminal.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+     Scanner sc= new Scanner(System.in);
+    System.out.println("Digite o numero de sua agencia ");
+    }
+}
